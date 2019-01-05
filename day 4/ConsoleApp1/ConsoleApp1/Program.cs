e3f26ef9c5ed0d37aeab0a1d316a3769d79ec700
Ejigsonpeter <ejigsonpeter@gmail.com>
2019-01-05 14:05:32 +0100

    x

--- day 4/ConsoleApp1/ConsoleApp1/Program.cs
+++ day 4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,16 +10,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a Target");
-            int val = int.Parse(Console.ReadLine());
-
-            int start = 0;
-            while (start <= val)
+            //while loop
+            string target = "";
+            do
             {
-                Console.WriteLine(start);
-                start += 2;
+                Console.WriteLine("Enter a Target");
+                int val = int.Parse(Console.ReadLine());
+
+                int start = 0;
+                while (start <= val)
+                {
+                    Console.WriteLine(start);
+                    start += 2;
+                }
+               
+                do
+                {
+                    //do while loop
+                    Console.WriteLine("Do you want to continue - Yes ot no");
+                    target = Console.ReadLine().ToUpper();
+                    if (target != "YES" && target != "NO")
+                    {
+                        Console.WriteLine("Invalid Choice , please Sat yes or no");
+                    }
+                    else if (target == "NO")
+                    {
+                        continue;
+                    }
+
+                }
+                while (target != "YES" && target != "NO");
             }
-           
+            while (target == "YES");
+
+
+
+
             Console.ReadKey();
 
             
---
+++ day 4/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[3];
+            array[0] = 3;
+            array[1] = 4;
+            array[2] = 7;
+            int i = 0;
+
+            for (int h = 0; i <=20; i++)
+            {
+                if(i % 2 == 1)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+            foreach (int k in array)
+            {
+                Console.WriteLine(k );
+            }
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine(array[j]);
+            }
+
+            while (i < array.Length)
+            {
+                Console.WriteLine(array[i]);
+                i++;
+            }
+            Console.ReadKey();
+
+            
+
+            
+        }
+    }
+}
