42efa72e3295bbfa1ae315ec1f1b5c63d187dfbc
ejigsonpeter <ejigsonpeter@gmail.com>
2019-01-14 15:58:27 +0100

    method hidingssss

--- day 13/methodhiding/Program.cs
+++ day 13/methodhiding/Program.cs
@@ -7,7 +7,30 @@ namespace csharpmethodhiding{
         public string email;
 
         //print method
-    
+        public void printFullName(){
+            Console.WriteLine(firstName +" "+lastName);
+        }
+
+    }
+    class fullTimeEmployee:Employee{
+
+    }
+    class partTimeEmployee:Employee{
+
+    }
+    class program{
+        public static void Main(string [] args){
+            fullTimeEmployee FTE = new fullTimeEmployee();
+            FTE.lastName = "Uyo";
+            FTE.lastName = "Meyo";
+            FTE.printFullName();
+
+            partTimeEmployee FTE = new partTimeEmployee();
+            PTE.lastName = "Uyo";
+            PTE.lastName = "Meyo";
+            PTE.printFullName();
+
+        }
     }
 }
 
