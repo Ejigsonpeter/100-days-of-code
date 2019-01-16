350bbd31d96bfa2c107d3fdea5a99e0dff1cb11f
ejigsonpeter <ejigsonpeter@gmail.com>
2019-01-16 13:59:47 +0100

    methods

--- day 14/methooddiff/Program.cs
+++ day 14/methooddiff/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+namespace vs{
+    class baseClass{
+        public virtual void print(){
+            Console.WriteLine("I am a Base Class");
+        }
 
-namespace methooddiff
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    }
+    class derrivedClass : baseClass{
+        public override void print(){
+            Console.WriteLine("I am a derrived class");
+        }
+
+    }
+    class program{
+        public static void Main(string [] args){
+            baseClass B = new derrivedClass();
+            B.print();
+
         }
     }
 }
