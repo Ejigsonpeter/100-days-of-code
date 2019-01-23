f1c45f0ed2b31e1190427f372088f9a6d4349b87
ejigsonpeter <ejigsonpeter@gmail.com>
2019-01-23 11:09:43 +0100

    days

--- day16/multipleinterface/Program.cs
+++ day16/multipleinterface/Program.cs
@@ -2,11 +2,47 @@
 
 namespace multipleinterface
 {
+
+interface IA
+{
+    void Amethod();
+}   
+class A : IA{
+    public void Amethod(){
+        Console.WriteLine("A");
+    }
+}
+interface IB
+{
+    void Bmethod();
+}   
+class B : IB{
+    public void Bmethod(){
+        Console.WriteLine("B");
+    }
+}
+
+class AB :IA,IB{
+
+    A a = new A();
+    B b = new B();
+   public void Amethod()
+{
+    Console.WriteLine("A");
+}
+public void Bmethod(){
+        Console.WriteLine("B");
+    }
+}
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AB ab = new AB();
+            ab.Amethod();
+            ab.Bmethod();
         }
     }
 }
---
+++ day18/delegates/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+// a delegate is a function pointer
+namespace delegates
+{
+    class Employee {
+        public int ID{set;get;}
+        public string Name{set;get;}
+        public int Salary {set;get;}
+        public int Expirience{set;get;}
+
+        public void PromoteEmployees(list <Employee> EmployeeList,isPromotable Iseligible){
+            for each(Employee employee in EmployeeList ){
+                if (employee.Expirience >= 5){
+                        Console.WriteLine(employee.Name +" Promoted");
+                }
+            }
+        }
+    }
+
+    public delegate void hellofunction(string Message);
+    public delegate bool isPromotable(Employee empl);
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            hellofunction del  = new hellofunction(hello);
+            del("hello");
+            List <employee> emplist = new List<employee>();
+            emplist.Add(new Employee{ID = 101, Name = "EJIGA PETER",Expirience = 5, Salary = 5000});
+            emplist.Add(new Employee{ID = 102, Name = "EJIGA PEACE",Expirience = 4, Salary = 4000});
+            emplist.Add(new Employee{ID = 103, Name = "EJIGA TIMOTHY",Expirience = 3, Salary = 3000});
+            emplist.Add(new Employee{ID = 104, Name = "EJIGA VICTOR",Expirience = 2, Salary = 2000});
+            emplist.Add(new Employee{ID = 105, Name = "EJIGA FRED",Expirience = 1, Salary = 1000});
+            //using inline lambda expression
+            Employee.PromoteEmployees(emplist,emp => 5);
+
+        }
+        
+
+        public static void hello(string strmessage){
+            Console.WriteLine(strmessage);
+        }
+    }
+}
